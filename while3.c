ac5bb9ede948616eb1741fa7aafbc2731e09ce80
Bryandlc12 <160089099+Bryandlc12@users.noreply.github.com>
2024-10-23 10:30:06 -0600

    while3.c

--- while3.c
+++ while3.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+
+int main (){
+    int numero0 = 1;
+    int numero1 = 0;
+    int numero2 = 0;
+    while (numero0 <= 500){
+        numero1 = numero0 * numero0;
+        numero2 = numero0 * numero0 * numero0;
+        printf("%d, \t %d, \t %d\n", numero0, numero1, numero2);
+        numero0++;
+    }
+    return 0;
+}
