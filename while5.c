622290266b924453a332876a2b2ae6b40a26cf10
Bryandlc12 <160089099+Bryandlc12@users.noreply.github.com>
2024-10-23 10:32:44 -0600

    while5.c

--- while5.c
+++ while5.c
@@ -5,3 +5,12 @@ int main (){
     int numero = 0;
     int n = 1;
     int l = 0;
+  while(numero<1000){
+        l=0;
+        l=numero/2/7;
+        if(l==n){
+            printf("%d, ",numero);
+            n=n*2;
+        }
+        numero++;
+    }
