23efb3fa6192848108d544c31a2b4c5e24d16c95
Bryandlc12 <160089099+Bryandlc12@users.noreply.github.com>
2024-10-23 10:30:56 -0600

    while4.c

---
+++ while4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    int numero = 0;
+    int numero1 = 0;
+
+    while (numero1<1000){
+        numero1 = numero *3;
+        printf("%d, ",numero1);
+        numero++;
+    }
