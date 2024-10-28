172e25ac6e03ffa53fbaa7bd876529c699338788
Bryan De la Cruz Terrazas <160089099+Bryandlc12@users.noreply.github.com>
2024-10-28 11:24:18 -0600

    Update Variable5.c

--- Variable5.c
+++ Variable5.c
@@ -5,3 +5,6 @@ int main(){
     int ent2 = 20;
     int ent3 = 2;
     int res;
+res = ent * ent2 * ent3;
+    printf("El resultado de multiplicar %d * %d * %d es : %d\t", ent, ent2, ent3, res);
+    printf("El resultado es: %d", res);
